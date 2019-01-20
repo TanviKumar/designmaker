0d822d1adca6a831bc92c43fb56964dfcdf5acba
TanviKumar <tanvikumar777@gmail.com>
2019-01-20 15:12:32 +0530

    Use variables instead of constants

--- squarespiral/sketch.js
+++ squarespiral/sketch.js
@@ -2,6 +2,8 @@ let start_angle = 0;
 let canvas_width = 800, canvas_height = 800;
 let current_width, current_height;
 let color1, color2, speed;
+let angle = 3.013;
+let side_factor = 0.945;
 
 function setup() {
   let myCanvas = createCanvas(canvas_width, canvas_height);
@@ -21,14 +23,14 @@ function draw() {
 		push();
 		noStroke();
 		translate(canvas_width/2, canvas_height/2);
-		rotate(start_angle + 3.013*i);
+		rotate(start_angle + angle * i);
 		if (i % 2)
 			fill(color(color1.value));
 		else
 			fill(color(color2.value));
 		rect(0, 0, current_width, current_height);
-		current_width = + (0.945 * current_width);
-		current_height = + (0.945 * current_height);
+		current_width = + (side_factor * current_width);
+		current_height = + (side_factor * current_height);
 		pop();
 	}
 
